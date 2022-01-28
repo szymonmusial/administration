d23a3a0a439fa843b70517e17ed439a9a7936a26
szymonmusial <szymonmusial.sm@gmail.com>
2022-01-28 14:33:25 +0100

    swap ButtonOpenModal to ts

--- src/components/organisms/ButtonOpenModal.vue
+++ src/components/organisms/ButtonOpenModal.vue
@@ -1,20 +1,26 @@
 <template>
-  <OwnButton
-    @click="editApplication"
-    :label="label"
-    :className="className"
-    :disabled="disabled"
-  />
+  <OwnButton @click="editApplication" :label="label" :className="className" :disabled="disabled" />
 </template>
 
-<script>
+<script lang="ts">
 import OwnButton from "../atoms/OwnButton.vue";
 import { useStore } from "vuex";
+import { modalType } from "@/store/modules/app/appTypes";
+import { PropType } from "@vue/runtime-core";
 
 export default {
   name: "ButtonEditApplication",
   components: { OwnButton },
-  props: ["id", "label", "editType", "className", "disabled"],
+  props: {
+    id: Number,
+    label: String,
+    editType: {
+      type: String as PropType<modalType>,
+      required: true,
+    },
+    className: String,
+    disabled: Boolean,
+  },
   setup(props) {
     const store = useStore();
     const editApplication = () => {
