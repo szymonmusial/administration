3c79cf273746147205f3813db36b1e873f9330bf
szymonmusial <szymonmusial.sm@gmail.com>
2022-01-28 15:20:09 +0100

    swap Modal list to ts

--- src/components/organisms/ModalListSubscriptions.vue
+++ src/components/organisms/ModalListSubscriptions.vue
@@ -1,18 +1,15 @@
 <template>
-  <Modal
-    @closeModal="closeModal"
-    title="Subscription List"
-    class="p-fluid p-formgrid p-grid basic-application-form"
-  >
+  <Modal @closeModal="closeModal" title="Subscription List" class="p-fluid p-formgrid p-grid basic-application-form">
     <Listbox :options="dataFromApplication.subscriptionList" />
   </Modal>
 </template>
 
-<script>
+<script lang="ts">
 import { computed } from "@vue/runtime-core";
 import Modal from "../atoms/Modal.vue";
 import { useStore } from "vuex";
 import Listbox from "primevue/listbox";
+import { Application } from "@/store/modules/application/applicationType";
 
 export default {
   name: "ModalListSubscriptions",
@@ -20,13 +17,9 @@ export default {
   setup(props, { emit }) {
     const store = useStore();
     const closeModal = () => emit("closeModal");
-    const applicationEditingId = computed(
-      () => store.getters.getEditingApplicationId
-    );
+    const applicationEditingId = computed((): Number => store.getters.getEditingApplicationId);
 
-    const dataFromApplication = computed(() =>
-      store.getters.getApplication(applicationEditingId.value)
-    );
+    const dataFromApplication = computed((): Application => store.getters.getApplication(applicationEditingId.value));
 
     return { closeModal, dataFromApplication };
   },
