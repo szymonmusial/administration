225c08f334f03ecb14bfabbca2b0a075eca45941
szymonmusial <szymonmusial.sm@gmail.com>
2022-01-28 15:23:43 +0100

    swap Modal Person to ts

--- src/components/organisms/ModalPersonEdit.vue
+++ src/components/organisms/ModalPersonEdit.vue
@@ -8,7 +8,7 @@
   </Modal>
 </template>
 
-<script>
+<script lang="ts">
 import Modal from "../atoms/Modal.vue";
 import FormVuelidateApplication from "./FormVuelidateApplication.vue";
 import { useStore } from "vuex";
@@ -17,6 +17,9 @@ import ownToast from "../../composables/ownToast/ownToast";
 import { required } from "@vuelidate/validators";
 import { reactive } from "@vue/reactivity";
 import { computed } from "@vue/runtime-core";
+
+import { Application, FormApplication, applicationStore } from "@/store/modules/application/applicationType";
+import { appStore } from "@/store/modules/app/appTypes";
 
 export default {
   name: "ModalPersonEdit",
@@ -37,27 +40,24 @@ export default {
       subscriptionList: {},
       person: { required },
     });
-    const applicationEditingId = computed(() => store.getters.getEditingApplicationId);
+    const applicationEditingId = computed((): Number => store.getters.getEditingApplicationId);
 
-    const dataFromApplication = computed(() => store.getters.getApplication(applicationEditingId.value));
+    const dataFromApplication = computed((): Application => store.getters.getApplication(applicationEditingId.value));
 
     const form = reactive({
       subscriptionList: dataFromApplication.value.subscriptionList,
       person: dataFromApplication.value.person,
     });
 
-    console.log(form);
-
-    const send = (data) => {
-      store.commit("setLoadingStatus", false);
+    const send = (data: FormApplication) => {
+      store.commit(appStore.commit.setLoadingStatus, false);
       const application = {
         id: applicationEditingId.value,
         subscriptionList: data.subscriptionList,
         person: data.person,
       };
-      debugger;
       store
-        .dispatch("editApplication", application)
+        .dispatch(applicationStore.dispatch.editApplication, application)
         .then(() => {
           showSuccessToast("Success", "Zmodyfikowane zadanie!");
           closeModal();
@@ -65,7 +65,7 @@ export default {
         .catch(() => {
           showErrorToast("Error", "Nie udało się zmodyfikować zadania");
         })
-        .finally(() => store.commit("setLoadingStatus", true));
+        .finally(() => store.commit(appStore.commit.setLoadingStatus, true));
     };
     return {
       closeModal,
